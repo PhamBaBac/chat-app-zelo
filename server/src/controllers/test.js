739196224b58cdff45dc43d2d798a47832312f6e
H82M19\Student <Student>
2024-03-25 11:01:49 +0700

    Cuong test

---
+++ server/src/controllers/test.js
@@ -0,0 +1,2 @@
+console.log("cuong")
+console.log("cuong")
---
+++ server/src/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default: "",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
+const UserModel = mongoose.model("users", userSchema);
+
+module.exports = UserModel;
